bf76d6bf6150ccd2c10e914c086e7238586c398c
Nayem Hossain <nayemsb12@gmail.com>
2026-07-09 18:50:26 +0600

    learned how to add optional field in Pydantic

--- pydantic/2.pydantic.py
+++ pydantic/2.pydantic.py
@@ -1,12 +1,14 @@
 from pydantic import BaseModel
-from typing import List,Dict
+from typing import List,Dict,Optional
+
+
 class Patient(BaseModel):
 
     name: str
     age: int
     weight: float
-    married: bool
-    allergies: List[str]
+    married: Optional[bool]
+    allergies: Optional[List[str]]
     contact_details: Dict[str, str]
 
 def insert_patient_data(patient : Patient):
@@ -17,6 +19,7 @@ def insert_patient_data(patient : Patient):
 def update_patient_data(patient : Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.allergies)
     print('Updated')
 
 patient_info = {'name':'Nayem','age':'30','weight': 84.2, 'married':False, 'allergies':['pollen','dust'], 'contact_details': {'email':'nayemsb12@gmail.com', 'phone':'2424234'}}
